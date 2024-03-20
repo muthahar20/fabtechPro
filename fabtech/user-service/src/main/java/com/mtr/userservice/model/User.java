4cb6fe58fab04deec34602bdcee4af7a10993073
muthahar20@gmail.com <muthahar20@gmail.com>
2024-03-20 11:37:35 -0400

    intial commit

---
+++ fabtech/user-service/src/main/java/com/mtr/userservice/model/User.java
@@ -0,0 +1,30 @@
+package com.mtr.userservice.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+	@Id
+	private long id;
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	
+
+}
